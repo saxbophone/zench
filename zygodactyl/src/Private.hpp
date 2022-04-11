fde714c8cd26a7c65e7674babdeaed031c116204
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-04-11 23:44:25 +0100

    Rename project

---
+++ zygodactyl/src/Private.hpp
@@ -0,0 +1,16 @@
+/*
+ * This is a sample private compilation unit.
+ *
+ * <Copyright information goes here>
+ */
+
+#ifndef COM_SAXBOPHONE_ZYGODACTYL_PRIVATE_HPP
+#define COM_SAXBOPHONE_ZYGODACTYL_PRIVATE_HPP
+
+#include <zygodactyl/Public.hpp>
+
+namespace com::saxbophone::zygodactyl::PRIVATE {
+    bool library_works();
+}
+
+#endif // include guard
