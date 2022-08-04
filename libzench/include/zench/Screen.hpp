7dd81ee434eb782e14516710d2a5ce3b6cbb8ca2
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-08-04 06:38:51 +0100

    Add ideas for how to spec the screen interface

--- libzench/include/zench/Screen.hpp
+++ libzench/include/zench/Screen.hpp
@@ -24,6 +24,29 @@
 namespace com::saxbophone::zench {
     class Screen : public Component {
         // This is meant to be abstract
+        /*
+         * This is meant to be like a low-level device driver, so we
+         * shouldn't make implementors worry about Z-machine concepts
+         * such as upper/lower window or numbered windows (when v6)
+         * as these vary across Z-machine versions anyway and will
+         * needlessly complicate the driver beyond what it needs to do.
+         * Let the Z-machine itself handle those concepts, including
+         * remembering where lines are for scrolling behaviour, etc...
+         *
+         * things to add here:
+         * - navigating the invisible cursor to a specific position
+         * - retrieving the screen dimensions
+         * - writing text starting at the cursor position, with styles
+         * - erase text from the cursor position to a specified end pos
+         * - erase an entire line
+         * - erase the whole screen
+         * - erase a single character
+         * - move a range of text from cursor to end pos, by a delta
+         * - get mouse position?
+         * - set the visible cursor position
+         * - change visible cursor style/blink settings/visibility
+         * - get resize events?
+         */
     };
 }
 
